056477039926809f739215fe0cf945d26543dee5
Yeahyea Sarker <1642cse00575@manarat.ac.bd>
2022-12-29 13:34:19 +0600

    Login and registration blade complete

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,48 @@
-@extends('layouts.app')
+@extends('layouts/signup')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="section-forgot-password">
+    <section class="section-forgot-password--logo">
+        <a href="{{ url ('/') }}">
+            <img src="{{ asset ('assets/images/logo.png') }}" alt="logo" class="img-fluid" />
+        </a>
+    </section>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <section class="section-forgot-password--form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-title">{{ __ ('Forgot Password') }}</div>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="mt-3">
+                                    <label for="email" class="form-label">{{ __ ('Email') }}</label>
+                                    <input id="email" type="email" class="form-control u-box-shadow-1 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                                    @enderror
+                                </div>
+                                <div class="my-5">
+                                    <button type="submit" class="btn btn-green">{{ __ ('Request Password') }}</button>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
+
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,49 +6,10 @@
      *  Time: 7:04 PM
      */
 ?>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+@extends('layouts/signup')
 
-    <meta name="description" content=""/>
-    <meta name="keywords" content="content"/>
-
-    <title>@yield('title')</title>
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/icons/favicon.ico') }}">
-    <!-- Bootstrap Icon CSS -->
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
-    />
-
-    <!-- Bootstrap CSS -->
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-    />
-
-    <!-- Google Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;700;800&display=swap"
-        rel="stylesheet"
-    />
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset ('assets/css/style.css') }}"/>
-
-</head>
-<body>
-
-<main class="section-register">
+@section('content')
+<div class="section-register">
     <section class="section-register--left">
         <div class="card">
             <div class="card-body">
@@ -132,19 +93,9 @@
             <a href="{{ url ('login') }}"> {{ __ ('Sign in') }} </a>
         </p>
     </section>
-</main>
+</div>
 
-<footer></footer>
-
-<!-- Bootstrap Bundle with Popper -->
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-></script>
-
-</body>
-</html>
+@endsection
 
 
 
