4e604e444a4fce4f21b9b2fe66804e8fc8444626
Yeahyea Sarker <1642cse00575@manarat.ac.bd>
2022-12-30 10:16:59 +0600

    web.php file simple edit

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('redirect', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth','verified']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['verified']);
 
 
